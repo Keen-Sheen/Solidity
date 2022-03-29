1a45cd26f6e89565725d980d5e7175d6da8c2701
KeenSheen <75229555+Keen-Sheen@users.noreply.github.com>
2022-03-28 20:01:46 -0400

    Update Meme_Coin.sol

--- Keffie Token/Meme_Coin.sol
+++ Keffie Token/Meme_Coin.sol
@@ -8,10 +8,10 @@
 
 /**
   
-   #BEE
+   #KFT
    
-   #LIQ+#RFI+#SHIB+#DOGE = #BEE
-   #SAFEMOON features:
+   #LIQ+#RFI+#SHIB+#DOGE+#SAFEMOON = #KFT
+   #Keffie features:
    3% fee auto add to the liquidity pool to locked forever when selling
    2% fee auto distribute to all holders
    I created a black hole so #Bee token will deflate itself in supply with every transaction
