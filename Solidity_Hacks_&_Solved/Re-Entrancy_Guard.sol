577e7c3c4c936ceafe532e045ef22fc6fdf10fd7
KeenSheen <75229555+Keen-Sheen@users.noreply.github.com>
2022-04-10 13:27:40 -0400

    Add files via upload

---
+++ Solidity_Hacks_&_Solved/Re-Entrancy_Guard.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.10;
+
+contract ReEntrancyGuard {
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+}
